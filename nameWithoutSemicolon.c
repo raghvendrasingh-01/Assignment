aa3e9d18000aef41a278d3e910738ef4ee22df3e
raghvendrasingh-01 <raghvendrasingh1811@gmail.com>
2025-09-30 19:32:53 +0530

    Add C program that prints name without semicolon

--- nameWithoutSemicolon.c
+++ nameWithoutSemicolon.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 void main()
 {
+    // Print the name and check if printf was successful
+    // printf returns the number of characters printed, which is always > 0 for non-empty strings
     if (printf("Raghvendra Singh\n")) {}
 }
